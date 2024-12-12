fa7b876da66dc115136ab5e06c6045ba173431f9
maybe-e <haluk.yilmaz300@gmail.com>
2024-12-12 19:53:18 +0100

    client qui peut envoyer et recevoir des messages

---
+++ src/chat/main.cpp
@@ -0,0 +1,20 @@
+#include <boost/asio.hpp>
+#include "client.hpp"
+#include <iostream>
+
+
+int main(int argc, char* argv[]) {
+   try {
+      boost::asio::io_context io_context;
+
+      // Initialise et démarre le client
+      Client client(io_context, "127.0.0.1", "1234");
+
+      // Démarre la boucle d'événements
+      io_context.run();
+   } catch (const std::exception& e) {
+      std::cerr << "Erreur : " << e.what() << "\n";
+   }
+
+   return 0;
+}
